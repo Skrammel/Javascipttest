6b15fd712d207105a8c79faf2421be7446f64ebb
Kim Remmel <Kim.Remmel@hotmail.com>
2017-11-21 10:35:45 +0100

    klar

--- Code.js
+++ Code.js
@@ -1,6 +1,8 @@
-﻿function rendermovie(movie) {
+﻿function renderMovie(movie) {
     document.getElementById("movietitle").innerText = movie.title;
     document.getElementById("movietext").innerText = movie.text;
-    document.getElementById("movieimg").innerText = movie.img;
+    document.getElementById("movieimg").src = movie.img;
+    for (let index = 0; index < movie.list.length; index++)
+    { let newRow = "<li>" + movie.list[index] + "</li>"; document.getElementById("movielist").innerHTML += newRow; }
 }
 renderMovie(movieData);
