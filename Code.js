b9ba8ff7f5883d9f71431a51aee7993b1849ace9
Kim Remmel <Kim.Remmel@hotmail.com>
2017-11-21 09:53:41 +0100

    test

---
+++ Code.js
@@ -0,0 +1,6 @@
+﻿function rendermovie(movie) {
+    document.getElementById("movietitle").innerText = movie.title;
+    document.getElementById("movietext").innerText = movie.text;
+    document.getElementById("movieimg").innerText = movie.img;
+}
+renderMovie(movieData);
